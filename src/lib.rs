c7ac4c2ca312625b1f40c7ac73862448a5a62d1b
AkinAguda <lordtitan7@gmail.com>
2021-10-06 01:21:24 +0100

    implemented gauss seidel

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,39 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+struct FluidConfig {
+    n: u16,
+    dt: u8,
+    diffusion: i32,
 }
 
+type PropertyType = Vec<f32>;
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, smoke-rust!");
+pub struct Fluid {
+    config: FluidConfig,
+    velocity_x: PropertyType,
+    velocityY: PropertyType,
+    initial_velocity_x: PropertyType,
+    initial_velocityY: PropertyType,
+    density: PropertyType,
+    initial_density: PropertyType,
+    size: u16,
+}
+
+impl Fluid {
+    fn new(config: FluidConfig) -> Fluid {
+        let size = (config.n + 2) * (config.n + 2);
+        let vector_size = size.into();
+        Fluid {
+            config,
+            velocity_x: vec![0.0; vector_size],
+            velocityY: vec![0.0; vector_size],
+            initial_velocity_x: vec![0.0; vector_size],
+            initial_velocityY: vec![0.0; vector_size],
+            density: vec![0.0; vector_size],
+            initial_density: vec![0.0; vector_size],
+            size,
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -8,3 +8,47 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+
+type LinearEquation<T> = fn(variables: &Vec<T>) -> T;
+
+pub fn gauss_seidel<T: Clone>(
+    functions: Vec<LinearEquation<T>>,
+    initial_values: Vec<T>,
+    iter: u16,
+) -> Vec<T> {
+    let mut inital_values_clone = initial_values.clone();
+    let mut iteration = 0;
+    while iteration < iter {
+        for (index, _) in initial_values.iter().enumerate() {
+            inital_values_clone[index] = functions[index](&inital_values_clone);
+        }
+        iteration += 1;
+    }
+    inital_values_clone
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gauss_seidel_works() {
+        fn fn1(variables: &Vec<f32>) -> f32 {
+            (3.0 + (2.0 * variables[1]) + variables[2] + variables[3]) / 10.0
+        }
+        fn fn2(variables: &Vec<f32>) -> f32 {
+            (15.0 + (2.0 * variables[0]) + variables[2] + variables[3]) / 10.0
+        }
+        fn fn3(variables: &Vec<f32>) -> f32 {
+            (27.0 + variables[0] + variables[1] + variables[3]) / 10.0
+        }
+        fn fn4(variables: &Vec<f32>) -> f32 {
+            ((-1.0 * 9.0) + variables[0] + variables[1] + (2.0 * variables[2])) / 10.0
+        }
+        let answers = gauss_seidel(vec![fn1, fn2, fn3, fn4], vec![0.0, 0.0, 0.0, 0.0], 10);
+        assert_eq!(answers[0], 1.0);
+        assert_eq!(answers[1], 2.0);
+        assert_eq!(answers[2], 3.0);
+        assert_eq!(answers[3], 0.0);
+    }
+}
