eaed1b410d0281fef1029ab06622e2624503d6c7
AkinAguda <lordtitan7@gmail.com>
2021-11-08 16:45:20 +0100

    fix 3

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -e "\e[1;33m BUILDING... Installing ans setting up cargo \e[0m"
-curl https://sh.rustup.rs -sSf | sh &&
+curl https://sh.rustup.rs -sSf -y | sh &&
 rustup install stable &&
 rustup default stable &&
 echo -e "\e[1;32m BUILDING... Cargo Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Installing wasm-pack \e[0m"
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh &&
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf -y | sh &&
 echo -e "\e[1;32m BUILDING... wasm-pack Installed sucessfully \e[0m"
 
 echo -e "\e[1;33m BUILDING... Building webassembly module \e[0m"
