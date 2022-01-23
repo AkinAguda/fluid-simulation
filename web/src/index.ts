727ca1da9b3f002ee2af0c2f5dc487da76355b05
AkinAguda <lordtitan7@gmail.com>
2022-01-23 12:59:34 +0100

    Adjusted config a bit

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,7 @@ import Renderer from "./renderer";
 
 import("fluid")
   .then(({ FluidConfig }) => {
-    const fluidConfig = FluidConfig.new(145, 0.5);
+    const fluidConfig = FluidConfig.new(135, 0.5);
     let renderer = new Renderer(fluidConfig, 0.6);
     renderer.start();
   })
